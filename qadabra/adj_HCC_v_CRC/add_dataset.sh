9b2d1a1d34069c956ae7866a3d3a2dd7ab443e84
Caitlin Guccione <cguccione1@gmail.com>
2024-09-17 14:02:50 -0700

    Delete qadabra directory
    Remove un-needed analysis

--- qadabra/adj_HCC_v_CRC/add_dataset.sh
+++
@@ -1,9 +0,0 @@
-qadabra add-dataset \
-    --workflow-dest ~/Dropbox/current_Fall23/HCC_Amir/HCC-microbialDNA/qadabra/git_install/qadabra/qadabra \
-    --table adj_HCC_v_CRC_filtered.biom \
-    --metadata ~/Dropbox/current_Fall23/HCC_Amir/HCC-microbialDNA/processed_data/metadata/metadata_adj_HCC_v_CRC.tsv \
-    --name adj_HCC_v_CRC_qiita15336_prep16181_pangenome_wol2_scrubbed_zebraFilter0.1 \
-    --factor-name tumor_type \
-    --target-level HCC \
-    --reference-level CRC \
-    --verbose
--- qadabra/blood_HCC_v_CRC/add_dataset.sh
+++
@@ -1,8 +0,0 @@
-qadabra add-dataset \
-    --workflow-dest /home/cguccion/packages/qadabra/my_qadabra \
-    --table adj_HCC_v_CRC_filtered.biom --metadata /panfs/cguccion/22_06_22_HCC_CRC_Amir/HCC-microbialDNA/processed_data/metadata/metadata_adj_HCC_v_CRC.tsv \
-    --name adj_HCC_v_CRC_qiita15336_prep16181_pangenome_wol2_scrubbed_zebraFilter0.1 \
-    --factor-name tumor_type \
-    --target-level HCC \
-    --reference-level CRC \
-    --verbose
