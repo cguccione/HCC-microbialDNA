312f97a9435073ce4e7bf72ccbe0fc396560ad3b
cguccione <cguccione1@gmail.com>
2024-09-17 11:01:49 -0700

    final figures for manuscript
    - Added checks in beta diversity for figure creation
    - Cleaned up analysis / figure notebook to be consistent for final figure building for Sep 2024 submission

--- python_scripts/alpha_beta_diversity.py
+++ python_scripts/alpha_beta_diversity.py
@@ -4,6 +4,7 @@ alpha and beta diversity
 '''
 
 import pandas as pd
+import numpy as np
 import subprocess
 import qiime2 as q2
 from qiime2 import Artifact, Metadata, Visualization
@@ -15,6 +16,7 @@ import biom
 from skbio.stats.distance import permanova
 from qiime2.plugins.diversity.pipelines import alpha
 from scipy.stats import mannwhitneyu
+from skbio import DistanceMatrix
 
 def alpha_rare_curve(table, max_depth, meta):
     
@@ -24,7 +26,7 @@ def alpha_rare_curve(table, max_depth, meta):
     
     return(alph_vis)
 
-def beta_decoide(ft, meta, metric, permutations, return_biplot=False):
+def beta_decoide(ft, meta, metric, permutations, fn, return_biplot=False):
     
     '''Calculates beta diversity for unweighted 
     & weighted unifraq
@@ -61,19 +63,52 @@ def beta_decoide(ft, meta, metric, permutations, return_biplot=False):
     
     #Save ft for command line processing
     ft.save('deicode_processing.qza')
-
+    
+    #''' $$$
+    if 'blood' in fn:
+        command = [
+        'qiime',
+        'deicode',
+        'rpca',
+        '--i-table',
+        'deicode_processing.qza',
+        '--p-min-sample-count',
+        '10', 
+        '--o-biplot',
+        'deicode_biplot.qza',
+        '--o-distance-matrix',
+        'deicode_distance_test.qza']
+    
+    else:
+        #tissue
+        command = [
+        'qiime',
+        'deicode',
+        'rpca',
+        '--i-table',
+        'deicode_processing.qza',
+        '--p-min-sample-count',
+        '2', 
+        '--p-min-feature-count',
+        '2',
+        '--o-biplot',
+        'deicode_biplot.qza',
+        '--o-distance-matrix',
+        'deicode_distance_test.qza']
+    #'''
+    
+    '''
+    ###Alternative to above with deicode .. now running bray-curtis
     command = [
-    'qiime',
-    'deicode',
-    'rpca',
-    '--i-table',
-    'deicode_processing.qza',
-    '--p-min-sample-count',
-    '10',
-    '--o-biplot',
-    'deicode_biplot.qza',
-    '--o-distance-matrix',
-    'deicode_distance_test.qza']
+        'qiime',
+        'diversity-lib',
+        'bray-curtis',
+        '--i-table',
+        'deicode_processing.qza',
+        '--o-distance-matrix',
+        'deicode_distance_test.qza']
+    ###
+    '''
 
     # Run the command
     subprocess.run(command)
@@ -81,8 +116,13 @@ def beta_decoide(ft, meta, metric, permutations, return_biplot=False):
     #Import biplot back into python
     rpca_biplot = Artifact.load('deicode_biplot.qza')
     
-    #Import biplot back into python
+    #Import distance matrix back into python
     rpca_distance_matrix = Artifact.load('deicode_distance_test.qza')
+    
+    #Create distance matrix df
+    temp_dm = rpca_distance_matrix.view(DistanceMatrix)
+    dm = temp_dm.to_data_frame()
+    #display(temp_df)
     
     #Calculate permanova 
     beta_result_d = diversity.actions.beta_group_significance(distance_matrix=rpca_distance_matrix,
@@ -90,10 +130,29 @@ def beta_decoide(ft, meta, metric, permutations, return_biplot=False):
                                                               method = 'permanova', pairwise = True,
                                                               permutations=permutations)
     
+    ### Calculate Avg. for each group in distance matrix this ###
+    # Filter rows and columns that contain 'HCC'
+    dm_hcc = dm.loc[dm.index.str.contains('HCC'), dm.columns.str.contains('HCC')]
+
+    # Filter rows and columns that contain 'CRC'
+    dm_crc = dm.loc[dm.index.str.contains('CRC'), dm.columns.str.contains('CRC')]
+    
+    # Create the leftover DataFrame by keeping all values but masking the ones that match HCC-HCC or CRC-CRC
+    mask_hcc = dm.index.str.contains('HCC')[:, None] & dm.columns.str.contains('HCC')
+    mask_crc = dm.index.str.contains('CRC')[:, None] & dm.columns.str.contains('CRC')
+    dm_leftovers = dm.copy()  # Create a copy to avoid modifying the original
+    dm_leftovers[mask_hcc] = np.nan
+    dm_leftovers[mask_crc] = np.nan    
+    
+    hcc_med = pd.Series(dm_hcc.values.flatten()).median()
+    crc_med = pd.Series(dm_crc.values.flatten()).median()
+    leftover_med = pd.Series(dm_leftovers.values.flatten()).median()
+    ###
+    
     if return_biplot == True:
         return(rpca_biplot)
                                                    
-    return(beta_result_d)  
+    return(beta_result_d, dm, hcc_med, crc_med, leftover_med)  
 
 
 def table_prep(table, rarefaction=None):
@@ -148,21 +207,21 @@ def rare_helper(vis_output, df):
     return(df)
 
 
-def rare_multiplier_fig(df_rs210_rpca_rare_genome, table_genome_rs210, meta, rarefaction, metric, decoide_min_feature_count, permutations=999):
+def rare_multiplier_fig(df_rs210_rpca_rare_genome, table_genome_rs210, meta, rarefaction, metric, decoide_min_feature_count, fn, permutations=999):
     
     #Convert meta to q2 object
-    sample_meta = q2.Metadata(meta.set_index('sample_name'))
+    #sample_meta = q2.Metadata(meta.set_index('sample_name'))
     
     #Rare Tables
     ft_genome_rs210_rare = table_prep(table_genome_rs210, rarefaction=rarefaction)
     
     #Rarefied RPCA
-    rs210_rpca_rare_genome = beta_decoide(ft_genome_rs210_rare, meta, metric, permutations, decoide_min_feature_count)
+    rs210_rpca_rare_genome, dm, hcc_med, crc_med, leftover_med = beta_decoide(ft_genome_rs210_rare, meta, metric, permutations, fn, decoide_min_feature_count)
     df_rs210_rpca_rare_genome = rare_helper(rs210_rpca_rare_genome, df_rs210_rpca_rare_genome)
     
-    return(df_rs210_rpca_rare_genome)
+    return(df_rs210_rpca_rare_genome, dm, hcc_med, crc_med, leftover_med)
 
-def beta_figures(table_genome_rs210,  meta, rarefaction, metric, permutations=999, numRares=10, decoide_min_feature_count=10, return_biplot=False):
+def beta_figures(table_genome_rs210,  meta, rarefaction, metric, fn, permutations=999, numRares=10, decoide_min_feature_count=10, return_biplot=False):
     
     '''Convert data into qiime2 objects
     and rarefy if needed
@@ -196,7 +255,7 @@ def beta_figures(table_genome_rs210,  meta, rarefaction, metric, permutations=99
     '''
     
     #Convert meta to q2 object
-    sample_meta = q2.Metadata(meta.set_index('sample_name'))
+    #sample_meta = q2.Metadata(meta.set_index('sample_name'))
     
     ##Create all Q2 Tables needed for Calculations##
     
@@ -204,16 +263,28 @@ def beta_figures(table_genome_rs210,  meta, rarefaction, metric, permutations=99
     
     #Create df to store averages across runs
     df_rs210_rpca_rare_genome = pd.DataFrame(columns = ['p-value', 'pseudo-F', 'Sample Size'])
+    
+    #Create a list to store all distance matrixes
+    dm_list = []
+    
+    #Create list to store the median values across distances matrixes
+    hcc_med_list = []
+    crc_med_list = []
+    leftover_med_list = []
         
     for i in range(0, numRares):
-        df_rs210_rpca_rare_genome = rare_multiplier_fig(df_rs210_rpca_rare_genome, table_genome_rs210, meta, 
-                                                        rarefaction, metric, decoide_min_feature_count, permutations=999)
+        df_rs210_rpca_rare_genome, dm, hcc_med, crc_med, leftover_med = rare_multiplier_fig(df_rs210_rpca_rare_genome, table_genome_rs210, meta, 
+                                                        rarefaction, metric, decoide_min_feature_count, fn, permutations=999)
+        dm_list.append(dm)
+        hcc_med_list.append(hcc_med)
+        crc_med_list.append(crc_med)
+        leftover_med_list.append(leftover_med)
 
     print('RS210 RPCA Rare Genome')
     display(df_rs210_rpca_rare_genome)
     print(df_rs210_rpca_rare_genome.mean())
     
-    return(df_rs210_rpca_rare_genome)
+    return(df_rs210_rpca_rare_genome, dm_list, hcc_med_list, crc_med_list, leftover_med_list)
     
 
 def alpha_rare_multiplier_fig(df_rs210_alpha_rare_genome, table_genome_rs210, meta, rarefaction, metric):
